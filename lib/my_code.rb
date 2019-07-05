54f8410f8a9c9c395583b9103eae391734f8f33a
Courtney Dyson <courtneydyson87@gmail.com>
2019-07-05 20:13:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -44,7 +44,7 @@ def reduce_to_total(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push(source_array[i].reduce)
+    new.push(source_array[i].reduce) { sum, n | sum + n  }
   end
   return new
 end
